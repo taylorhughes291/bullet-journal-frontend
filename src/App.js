2aab5e898c0b783c048f6ca59f515cf7167d3236
Taylor Hughes <taylor hughes>
2021-06-16 15:30:41 -0700

    implemented routing system

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Add from "./pages/Add"
+import Create from "./pages/Create"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Month from "./pages/Month"
+import Week from "./pages/Week"
+import Year from "./pages/Year"
+import {Switch, Route, Redirect, withRouter} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Add />
+      <Switch>
+        <Route
+          exact path="/"
+        >
+          <Redirect to="/login" />
+        </Route>
+        <Route
+          path="/add"
+        >
+          <Add />
+        </Route>
+        <Route
+          path="/create"
+        >
+          <Create />
+        </Route>
+        <Route
+          path="/home"
+        >
+          <Home />
+        </Route>
+        <Route
+          path="/login"
+        >
+          <Login />
+        </Route>
+        <Route
+          path="/month"
+        >
+          <Month />
+        </Route>
+        <Route
+          path="/week"
+        >
+          <Week />
+        </Route>
+        <Route
+          path="/year"
+        >
+          <Year />
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
