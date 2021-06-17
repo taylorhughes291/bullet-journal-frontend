1d41c57b6aed7e2f5646fb3526451b31cc3f11f0
Taylor Hughes <taylor hughes>
2021-06-17 15:28:42 -0700

    saving changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Add from "./pages/Add"
 import Create from "./pages/Create"
@@ -9,8 +8,64 @@ import Week from "./pages/Week"
 import Year from "./pages/Year"
 import Nav from "./components/Nav"
 import {Switch, Route, Redirect, withRouter} from "react-router-dom"
+import {useState, useEffect} from "react"
 
 function App() {
+
+  /////////////////////////
+  // Constants
+  /////////////////////////
+
+  const [userId, setUserId] = useState("2")
+  const [date, setDate] = useState("")
+  const [tasks, setTasks] = useState([])
+  const [events, setEvents] = useState([])
+
+  const url = process.env.REACT_APP_BACKENDURL
+
+  /////////////////////////
+  // Functions
+  /////////////////////////
+
+  const getData = () => {
+    const getUrl = url + "/user/" + userId
+    console.log(getUrl);
+    fetch(getUrl)
+    .then((response) => (response.json()))
+    .then((data) => {
+      setTasks(data.data.tasks)
+      setEvents(data.data.events)
+    })
+  }
+  
+  const updateTask = () => {
+
+  }
+
+  const updateEvent = () => {
+
+  }
+
+  const deleteUser = () => {
+
+  }
+
+  const deleteTask = () => {
+
+  }
+
+  const deleteEvent = () => {
+
+  }
+
+  /////////////////////////
+  // Render
+  /////////////////////////
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className="App">
       <Switch>
@@ -27,32 +82,48 @@ function App() {
         <Route
           path="/create"
         >
-          <Create />
+          <Create 
+            setUserId={setUserId}
+          />
         </Route>
         <Route
           path="/home"
         >
-          <Home />
+          <Home 
+            tasks={tasks}
+            events={events}
+          />
         </Route>
         <Route
           path="/login"
         >
-          <Login />
+          <Login 
+            setUserId={setUserId}
+          />
         </Route>
         <Route
           path="/month"
         >
-          <Month />
+          <Month 
+            tasks={tasks}
+            events={events}
+          />
         </Route>
         <Route
           path="/week"
         >
-          <Week />
+          <Week 
+            tasks={tasks}
+            events={events}
+          />
         </Route>
         <Route
           path="/year"
         >
-          <Year />
+          <Year 
+            tasks={tasks}
+            events={events}
+          />
         </Route>
       </Switch>
       <Nav />
