7eb61c71634213a30f5bc120339153a418cfaa8c
Taylor Hughes <taylor hughes>
2021-06-17 17:06:52 -0700

    completed all app-level functions

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   // Constants
   /////////////////////////
 
-  const [userId, setUserId] = useState("2")
+  const [userId, setUserId] = useState("3")
   const [date, setDate] = useState("")
   const [tasks, setTasks] = useState([])
   const [events, setEvents] = useState([])
@@ -29,33 +29,76 @@ function App() {
 
   const getData = () => {
     const getUrl = url + "/user/" + userId
-    console.log(getUrl);
     fetch(getUrl)
     .then((response) => (response.json()))
     .then((data) => {
-      setTasks(data.data.tasks)
-      setEvents(data.data.events)
+      setTasks(data.tasks)
+      setEvents(data.events)
     })
   }
   
-  const updateTask = () => {
-
+  const updateTask = (body, id) => {
+    const putUrl = url + "/task/?id=" + id + "&user=" + userId
+    fetch(putUrl, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      setTasks(data);
+    })
   }
 
-  const updateEvent = () => {
 
+  const updateEvent = (body, id) => {
+    const putUrl = url + "/event/?id=" + id + "&user=" + userId
+    fetch(putUrl, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      setEvents(data);
+    })
   }
 
   const deleteUser = () => {
-
+    const deleteUrl = url + "/user/?id=" + userId
+    fetch(deleteUrl, {
+      method: "DELETE"
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
   }
 
-  const deleteTask = () => {
-
+  const deleteTask = (id) => {
+    const deleteUrl = url + "/task/?id=" + id + "&user=" + userId
+    fetch(deleteUrl, {
+      method: "DELETE"
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      setTasks(data)
+    })
   }
 
-  const deleteEvent = () => {
-
+  const deleteEvent = (id) => {
+    const deleteUrl = url + "/event/?id=" + id + "&user=" + userId
+    fetch(deleteUrl, {
+      method: "DELETE"
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      setEvents(data)
+    })
   }
 
   /////////////////////////
@@ -92,6 +135,10 @@ function App() {
           <Home 
             tasks={tasks}
             events={events}
+            updateTask={updateTask}
+            updateEvent={updateEvent}
+            deleteTask={deleteTask}
+            deleteEvent={deleteEvent}
           />
         </Route>
         <Route
@@ -107,6 +154,10 @@ function App() {
           <Month 
             tasks={tasks}
             events={events}
+            updateTask={updateTask}
+            updateEvent={updateEvent}
+            deleteTask={deleteTask}
+            deleteEvent={deleteEvent}
           />
         </Route>
         <Route
@@ -115,6 +166,10 @@ function App() {
           <Week 
             tasks={tasks}
             events={events}
+            updateTask={updateTask}
+            updateEvent={updateEvent}
+            deleteTask={deleteTask}
+            deleteEvent={deleteEvent}
           />
         </Route>
         <Route
@@ -123,6 +178,10 @@ function App() {
           <Year 
             tasks={tasks}
             events={events}
+            updateTask={updateTask}
+            updateEvent={updateEvent}
+            deleteTask={deleteTask}
+            deleteEvent={deleteEvent}
           />
         </Route>
       </Switch>
