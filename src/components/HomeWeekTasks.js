4609fe81962e0d9fc688e2455fd1fa5d2fcd1845
Taylor Hughes <taylor hughes>
2021-06-20 16:52:54 -0700

    rendered homeweektasks

--- src/components/HomeWeekTasks.js
+++ src/components/HomeWeekTasks.js
@@ -1,10 +1,28 @@
 import React from "react"
+import Moment from "react-moment"
+import moment from "moment"
 
-const HomeWeekTasks = () => {
+const HomeWeekTasks = (props) => {
 
     /////////////////////////
     // Constants
     /////////////////////////
+
+    const weekTasks = props.tasks.filter((item, index) => {
+        const todayDate = moment.utc(props.date)
+        return (
+            moment(item.fields.dueDate).isSame(todayDate, "week") && item.fields.taskCycle.week && !item.fields.isComplete
+        )
+    })
+
+    const taskList = weekTasks.map((item, index) => {
+        return (
+            <div className="today-task-cont" key={index}>
+                <p>{item.fields.name}</p>
+                <p>...</p>
+            </div>
+        )
+    })
 
     /////////////////////////
     // Functions
@@ -15,7 +33,15 @@ const HomeWeekTasks = () => {
     /////////////////////////
 
     return (
-        <h2>HomeWeekTasks Page</h2>
+        <>
+        <div>
+            <p>Tasks</p>
+            <button>+</button>
+        </div>
+        <div className="today-tasks-cont">
+            {taskList}
+        </div>
+    </>
     )
 }
 
