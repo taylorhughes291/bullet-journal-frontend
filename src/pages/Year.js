4e75240c717cc98701cd9bc63cfa54591e9e8a5f
Taylor Hughes <taylor hughes>
2021-06-20 23:51:41 -0700

    added calndar and month rendering

--- src/pages/Year.js
+++ src/pages/Year.js
@@ -1,7 +1,7 @@
 import React from "react"
 import YearMonths from "../components/YearMonths"
 
-const Year = () => {
+const Year = (props) => {
 
     /////////////////////////
     // Constants
@@ -17,8 +17,19 @@ const Year = () => {
 
     return (
         <>
-            <h2>Year Page</h2>
-            <YearMonths />
+            <div>
+                <img src="https://placeimg.com/100/50/any" alt="small logo" />
+                <img src="https://placeimg.com/50/50/any" alt="hamburger options menu" />
+            </div>
+            <YearMonths 
+                tasks={props.tasks}
+                events={props.events}
+                updateTask={props.updateTask}
+                updateEvent={props.updateEvent}
+                deleteTask={props.deleteTask}
+                deleteEvent={props.deleteEvent}
+                date={props.date}
+            />
         </>
     )
 }
